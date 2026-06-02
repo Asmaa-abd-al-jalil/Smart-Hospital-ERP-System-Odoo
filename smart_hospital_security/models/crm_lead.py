8e773ed47f1ea6c70fc0ee8e52dc3459fd57534b
Asmaa-abd-al-jalil <asmaamh151@gmail..com>
2026-06-02 19:19:28 +0300

    CU-86ba8dju7_Phase-1-Module-Scaffolding-ORM-Overriding-Python

--- smart_hospital_security/models/crm_lead.py
+++ smart_hospital_security/models/crm_lead.py
@@ -3,23 +3,36 @@ from odoo import models, fields, api, _
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    # 1. تعريف الحقول أولاً!
     patient_heart_rate = fields.Integer(string="Patient Heart Rate")
     is_critical_iot_alert = fields.Boolean(string="Is Critical Alert", default=False)
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            self._check_critical_vitals(vals)
+        return super().create(vals_list)
+
     def write(self, vals):
-        # 2. المنطق البرمجي
+        self._check_critical_vitals(vals)
+        return super().write(vals)
+
+    def _check_critical_vitals(self, vals):
         if 'patient_heart_rate' in vals:
             heart_rate = vals['patient_heart_rate']
             
             if heart_rate < 40 or heart_rate > 140:
-                vals['priority'] = '3' 
-                vals['is_critical_iot_alert'] = True
-                
-                self.message_post(
+                vals.update({
+                    'priority': '3',
+                    'is_critical_iot_alert': True
+                })
+                self.sudo().message_post(
                     body=_("SYSTEM OVERRIDE: CRITICAL VITALS DETECTED."),
                     message_type='notification',
                     subtype_xmlid='mail.mt_note'
                 )
-        
-        return super(CrmLead, self).write(vals)
+            
+            else:
+                vals.update({
+                    'priority': '0', 
+                    'is_critical_iot_alert': False
+                })
